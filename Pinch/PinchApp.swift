cfd3905da9d81ce324c48235f9f940cc356fe270
umut9191 <umutsurmeli91@gmail.com>
2022-02-27 19:39:15 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Macintosh on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
